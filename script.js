d4d529d6ec8fcc0bbc3b345f1418629b440a8581
Filipe Avelino Montoto <filipe.montoto@kessem.com.br>
2025-07-18 15:21:59 -0300

    chore: first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+const destino = new Date("2025-12-06T00:00:00").getTime();
+const x = setInterval(function () {
+    const agora = new Date().getTime();
+    const distancia = destino - agora;
+
+    const dias = Math.floor(distancia / (1000 * 60 * 60 * 24));
+    const horas = Math.floor((distancia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutos = Math.floor((distancia % (1000 * 60 * 60)) / (1000 * 60));
+    const segundos = Math.floor((distancia % (1000 * 60)) / 1000);
+
+    document.getElementById("contador").innerHTML =
+        `${dias}d ${horas}h ${minutos}m ${segundos}s`;
+
+    if (distancia < 0) {
+        clearInterval(x);
+        document.getElementById("contador").innerHTML = "É HOJE! 💖";
+    }
+}, 1000);
